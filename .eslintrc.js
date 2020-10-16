a2c995140f66e96cccb301d90491f905b0df0b6d
Julian Nicholls <juliannicholls29@gmail.com>
2020-10-16 21:26:28 +0100

    Update eslint installation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
