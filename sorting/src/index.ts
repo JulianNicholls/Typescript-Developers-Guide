2a0f940344f3b0e6fc72241bbe6740311c3474f9
Julian Nicholls <juliannicholls29@gmail.com>
2020-10-18 17:49:20 +0100

    Initial bubble sort implementation

--- sorting/src/index.ts
+++ sorting/src/index.ts
@@ -1,7 +1,33 @@
-console.log('Hi there');
+class Sorter {
+  constructor(private _collection: number[]) { }
 
-const logSomething = () => {
-  console.log('Something else');
-};
+  get collection() {
+    return this._collection;
+  }
 
-logSomething();
+  sort(): void {
+    const { length } = this._collection;
+
+    for (let i = 0; i < length; ++i) {
+      for (let j = 0; j < length - i - 1; ++j) {
+        if (this._collection[j] > this._collection[j + 1]) {
+          this.swap(j);
+        }
+      }
+    }
+  }
+
+  // Swap number at index idx with number at idx + 1
+  private swap(idx: number) {
+    const left = this._collection[idx];
+    this._collection[idx] = this._collection[idx + 1];
+    this._collection[idx + 1] = left;
+  }
+}
+
+const numbers = [5, -1, 0, 10, 12, 7];
+
+const sorter = new Sorter(numbers);
+
+sorter.sort();
+console.log(sorter.collection);
