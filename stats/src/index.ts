59ed11b603fe74f4d36112abc59f29c881604a2b
Julian Nicholls <juliannicholls29@gmail.com>
2020-10-19 14:32:41 +0100

    Create first 'bad' code for Stats

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Stats started');
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+})
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+let manUWins = 0;
+
+matches.forEach(match => {
+  if (match[1] === 'Man United' && match[5] === 'H' ||
+    match[2] === 'Man United' && match[5] === 'A')
+    ++manUWins;
+});
+
+console.log(`Manchester United won ${manUWins} times.`);
