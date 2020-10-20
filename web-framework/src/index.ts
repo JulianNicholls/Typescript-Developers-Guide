7a6481d317265a8919c343ee70713664510d31a6
Julian Nicholls <juliannicholls29@gmail.com>
2020-10-20 10:45:56 +0100

    Add save() to User class

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,10 +1,8 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { User } from './models/User';
 
-const user = new User({ name: 'Julian', age: 23 });
+// const baseURL = 'http://localhost:3100/users';
 
-const baseURL = 'http://localhost:3100/users';
+const user = new User({ name: 'Tara', age: 23 });
 
-const users = await axios.get(baseURL);
-
-console.log(users);
+user.save();
--- web-framework/src/models/User.ts
+++ web-framework/src/models/User.ts
@@ -1,9 +1,14 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
 
 type Callback = () => void;
+
+const baseURL = 'http://localhost:3100/users';
 
 export class User {
   constructor(private data: UserProps) { }
@@ -16,6 +21,21 @@ export class User {
     this.data = { ...this.data, ...update };
 
     this.trigger('change');
+  }
+
+  fetch(): void {
+    axios.get(`${baseURL}/${this.get('id')}`).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    const id = this.get('id');
+
+    if (id)
+      axios.put(`${baseURL}/${id}`, this.data);
+    else
+      axios.post(baseURL, this.data);
   }
 
   on(eventName: string, callback: Callback): void {
