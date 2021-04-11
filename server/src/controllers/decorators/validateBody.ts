9eb68a6004d2bfc9f3bf11c407e85e8f259552ee
Julian Nicholls <juliannicholls29@gmail.com>
2021-04-11 14:39:37 +0100

    Add body validator

--- server/src/controllers/decorators/validateBody.ts
+++ server/src/controllers/decorators/validateBody.ts
@@ -3,7 +3,7 @@ import 'reflect-metadata';
 import { MetadataKeys } from './MetadataKeys';
 
 export function validateBody(...keys: string[]) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
     Reflect.defineMetadata(MetadataKeys.validator, keys, target, key);
   };
 }
